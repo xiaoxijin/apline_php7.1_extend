1ff024fdfff97a10fbd123f12182d59957050b03
xiaoxijin <xiaoxijin23@126.com>
2017-03-06 15:29:47 +0800

    php7.1构建失败，根据日志改成php7测试

--- build_extend.sh
+++ build_extend.sh
@@ -1,15 +1,11 @@
-#echo "http://nl.alpinelinux.org/alpine/latest-stable/main" > /etc/apk/repositories
-#echo "http://nl.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories
-#echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
-
 apk update && apk upgrade \
 && apk add autoconf build-base linux-headers \
 libaio-dev \
 zlib-dev \
-php7.1-dev \
-php7.1-pear \
-&& ln -s /usr/bin/php-config7.1 /usr/bin/php-config \
-&& ln -s /usr/bin/phpize7.1 /usr/bin/phpize \
+php-dev \
+php-pear \
+&& ln -s /usr/bin/php-config /usr/bin/php-config \
+&& ln -s /usr/bin/phpize /usr/bin/phpize \
 && sed -i "s/struct sigaction {/#ifndef __sighandler_t \ntypedef void (*__sighandler_t)(int);\n#endif\nstruct sigaction\n{/g" /usr/include/signal.h \
 && sed -i "s/union {void (*sa_handler)(int)/__sighandler_t sa_handler/g" /usr/include/signal.h \
 && sed -i "s/ -n / /" `which pecl` \
@@ -25,10 +21,7 @@ php7.1-pear \
 && apk del \
 zlib-dev \
 libaio-dev \
-php7.1-dev \
-php7.1-pear \
+php-dev \
+php-pear \
 autoconf build-base linux-headers \
 && rm -rf /var/cache/apk && mkdir /var/cache/apk/ && rm -rf /tmp/*
-
-## && cp /usr/lib/php7/modules/swoole.so /root/swoole.so \
-## mv /root/swoole.so /usr/lib/php7/modules/swoole.so
