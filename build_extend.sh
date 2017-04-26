0e730c1ac42ade632e5dde8103790c13197996c5
xiaoxijin <xiaoxijin23@126.com>
2017-04-26 15:24:43 +0800

    自动安装openssl

--- build_extend.sh
+++ build_extend.sh
@@ -1,16 +1,5 @@
-#echo "http://nl.alpinelinux.org/alpine/latest-stable/main" > /etc/apk/repositories
-#echo "http://nl.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories
-#echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
-
 
 apk update && apk upgrade
-apk add openssl openssl-dev
-apk add autoconf build-base linux-headers \
-libaio-dev \
-zlib-dev \
-pcre-dev \
-php7-dev \
-php7-pecl
 
 apk add php7-opcache \
 php7-session \
@@ -22,26 +11,32 @@ php7-posix \
 php7-ctype
 
 
+apk add autoconf build-base linux-headers \
+libaio-dev \
+zlib-dev \
+pcre-dev \
+php7-dev \
+php7-pecl
 
-#
-#ln -s /usr/bin/php-config7 /usr/bin/php-config \
-#&& ln -s /usr/bin/phpize7 /usr/bin/phpize \
-#&& ln -s /usr/bin/pecl7 /usr/bin/pecl
-#
-#pecl install swoole \
-#&& pecl install yaconf \
-#&& pecl install channel://pecl.php.net/yac-2.0.1 \
-#&& pecl install xdebug \
-#&& pecl install ds \
-#&& pecl install inotify \
-#&& pecl install msgpack \
-#&& pecl install yaf \
-#&& pecl install redis
-#
-#apk del autoconf build-base linux-headers \
-#libaio-dev \
-#zlib-dev \
-#pcre-dev \
-#php7-dev \
-#php7-pecl \
-#&& rm -rf /var/cache/apk && mkdir /var/cache/apk/ && rm -rf /tmp/*
+
+ln -s /usr/bin/php-config7 /usr/bin/php-config \
+&& ln -s /usr/bin/phpize7 /usr/bin/phpize \
+&& ln -s /usr/bin/pecl7 /usr/bin/pecl
+
+pecl install swoole \
+&& pecl install yaconf \
+&& pecl install channel://pecl.php.net/yac-2.0.1 \
+&& pecl install xdebug \
+&& pecl install ds \
+&& pecl install inotify \
+&& pecl install msgpack \
+&& pecl install yaf \
+&& pecl install redis
+
+apk del autoconf build-base linux-headers \
+libaio-dev \
+zlib-dev \
+pcre-dev \
+php7-dev \
+php7-pecl \
+&& rm -rf /var/cache/apk && mkdir /var/cache/apk/ && rm -rf /tmp/*
