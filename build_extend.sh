2e5ae9fee3e989c8d1180b9d2fc339405bc6b898
xiaoxijin <xiaoxijin23@126.com>
2017-04-26 13:59:35 +0800

    测试默认扩展安装优先级

--- build_extend.sh
+++ build_extend.sh
@@ -14,34 +14,32 @@ php7-mysqli \
 php7-posix \
 php7-ctype
 
-apk add autoconf build-base linux-headers \
-libaio-dev \
-zlib-dev \
-pcre-dev \
-php7-dev \
-php7-pecl
+#apk add autoconf build-base linux-headers \
+#libaio-dev \
+#zlib-dev \
+#pcre-dev \
+#php7-dev \
+#php7-pecl
 
-
-ln -s /usr/bin/php-config7 /usr/bin/php-config \
-&& ln -s /usr/bin/phpize7 /usr/bin/phpize \
-&& ln -s /usr/bin/pecl7 /usr/bin/pecl \
-#sed -i "s/struct sigaction {/#ifndef __sighandler_t \ntypedef void (*__sighandler_t)(int);\n#endif\nstruct sigaction\n{/g" /usr/include/signal.h \
-#&& sed -i "s/union {void (*sa_handler)(int)/__sighandler_t sa_handler/g" /usr/include/signal.h \
-#&& sed -i "s/ -n / /" `which pecl` \
-pecl install swoole \
-&& pecl install yaconf \
-&& pecl install channel://pecl.php.net/yac-2.0.1 \
-&& pecl install xdebug \
-&& pecl install ds \
-&& pecl install inotify \
-&& pecl install msgpack \
-&& pecl install yaf \
-&& pecl install redis
-
-apk del autoconf build-base linux-headers \
-libaio-dev \
-zlib-dev \
-pcre-dev \
-php7-dev \
-php7-pecl \
-&& rm -rf /var/cache/apk && mkdir /var/cache/apk/ && rm -rf /tmp/*
+#
+#ln -s /usr/bin/php-config7 /usr/bin/php-config \
+#&& ln -s /usr/bin/phpize7 /usr/bin/phpize \
+#&& ln -s /usr/bin/pecl7 /usr/bin/pecl
+#
+#pecl install swoole \
+#&& pecl install yaconf \
+#&& pecl install channel://pecl.php.net/yac-2.0.1 \
+#&& pecl install xdebug \
+#&& pecl install ds \
+#&& pecl install inotify \
+#&& pecl install msgpack \
+#&& pecl install yaf \
+#&& pecl install redis
+#
+#apk del autoconf build-base linux-headers \
+#libaio-dev \
+#zlib-dev \
+#pcre-dev \
+#php7-dev \
+#php7-pecl \
+#&& rm -rf /var/cache/apk && mkdir /var/cache/apk/ && rm -rf /tmp/*
